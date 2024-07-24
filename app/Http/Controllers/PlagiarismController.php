2d9cee493d9f8420c2322a5fa3e835d136d8083e
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-07-24 12:47:18 +0700

    feat: set locale to id

--- app/Http/Controllers/PlagiarismController.php
+++ app/Http/Controllers/PlagiarismController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Document;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PlagiarismController extends Controller
 {
     public function index()
     {
-        return view('plagiarism.index', ['title' => "Cek Plagiasi", 'documents' => Document::all()]);
+        $documents = Document::latest()->paginate(5)->map(function ($doc) {
+            $doc->creation_date = Carbon::parse($doc->creation_date)->translatedFormat('d F Y');
+            $doc->mod_date = Carbon::parse($doc->mod_date)->translatedFormat('d F Y');
+
+            return $doc;
+        });
+
+        return view('plagiarism.index', ['title' => "Cek Plagiasi", 'documents' => $documents]);
     }
 
     public function details(Document $document)
